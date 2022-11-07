2e7ebc06d3fd3325d1e8e86b3a0a1925d215e0c3
mamskabbaj <kabbajmamoun1@gmail.com>
2022-11-07 18:02:39 +0100

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+has_many :events, through: :attendances
+validates :email, presence: true
+#validates :encrypted_password, presence: true
+validates :description, presence: true
+validates :first_name, presence: true
+validates :last_name, presence: true
+after_create :welcome_send
+
+def welcome_send
+  UserMailer.welcome_email(self).deliver_now
+end
+end
